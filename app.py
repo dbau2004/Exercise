fd13ecf851774144136cd692c7453a6122b0e809
Đặng Duy Báu <duybau2004.com>
2025-09-24 09:03:34 +0700

    fix giao dien

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
 
-
+# API check tình trạng server
 @app.get('/health')
 def health():
-	return 'OK', 200
+    return 'OK', 200
 
+# API trả về JSON
+@app.get('/api')
+def hello_api():
+    return jsonify(msg='Hello from Flask API!')
 
+# Trang giao diện chính (HTML)
 @app.get('/')
-def hello():
-	return jsonify(msg='Hello from Flask!')
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
